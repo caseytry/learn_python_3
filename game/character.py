cf78392f446c0cfa00e0e19ac5c122f1ddc8954a
caseytry <casey.trytten@gmail.com>
2022-08-03 20:20:26 -0700

    initial thoughts of inventory system

--- game/character.py
+++ game/character.py
@@ -1,2 +1,24 @@
 class Character(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.inventory = {}
+
+    def additem(self, item):
+        self.inventory[str(item)]
+    
+    def deleteitem(self, item):
+        del self.inventory[str(item)]
+
+    def displayinventory(self):
+        return self.inventory
+
+
+player = Character(input("Name your character!\n>>"))
+print(f"You named them {player.name}")
+print(f"player is a {player} class")
+
+print(player.displayinventory)
+#weapon = "sword"
+player.additem('weapon': "sword")
+
+print(player.inventory)
