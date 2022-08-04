98c964c5f5d8c8b48f2452632a0ccdce55817fd6
caseytry <casey.trytten@gmail.com>
2022-08-03 20:52:14 -0700

    not working yet. circular importing.

--- game/game.py
+++ game/game.py
@@ -1,5 +1,5 @@
 import character
-
+import engine
 
 player = character.Character(input("Name your character!\n>>"))
 print(f"You named them {player.name}")
@@ -10,3 +10,7 @@ print(f"do you have the key? {player.has_key}")
 player.has_key = True
 
 print(f"do you have the key now? {player.has_key}")
+
+a_map = engine.Map('firstroom')
+a_game= engine.Engine(a_map)
+a_game.play()
