f2b3c6d98db44b3c474a63318c00b563566afa49
caseytry <casey.trytten@gmail.com>
2022-08-13 01:05:58 -0700

    importing engine and character through game

--- game/engine.py
+++ game/engine.py
@@ -1,5 +1,5 @@
 import character
-import game
+
 
 class Engine(object):
 
@@ -16,35 +16,3 @@ class Engine(object):
 
         #print out last scene
         current_scene.enter()
-
-class Scene(object):
-
-    def enter(self):
-        print("This scene not configured yet.")
-        print("Subclass it and implement enter().")
-        exit(1)
-
-class FirstRoom(Scene):
-
-    def enter(self):
-        print("we are in the first room!")
-        print(f"{player.name} key statuts is {player.has_key}")
-        return exit
-
-
-class Map(object):
-
-    scenes = {
-        'firstroom': FirstRoom()
-    }
-
-    def __init__(self, start_scene):
-        self.start_scene = start_scene
-
-    def next_scene(self, scene_name):
-        val = Map.scenes.get(scene_name)
-        return val
-
-    def opening_scene(self):
-        return self.next_scene(self.start_scene)
-
--- game/game.py
+++ game/game.py
@@ -1,6 +1,36 @@
 import character
 import engine
 
+class Scene(object):
+
+    def enter(self):
+        print("This scene not configured yet.")
+        print("Subclass it and implement enter().")
+        exit(1)
+
+class FirstRoom(Scene):
+
+    def enter(self):
+        print("we are in the first room!")
+        print(f"{player.name} key statuts is {player.has_key}")
+        return exit
+
+class Map(object):
+
+    scenes = {
+        'firstroom': FirstRoom()
+    }
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+
 player = character.Character(input("Name your character!\n>>"))
 print(f"You named them {player.name}")
 print(f"player is a {player} class")
@@ -11,6 +41,6 @@ player.has_key = True
 
 print(f"do you have the key now? {player.has_key}")
 
-a_map = engine.Map('firstroom')
+a_map = Map('firstroom')
 a_game= engine.Engine(a_map)
 a_game.play()
