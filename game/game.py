ddb4d3410269874caa2ebcf41290059d0f282513
caseytry <casey.trytten@gmail.com>
2022-08-13 17:17:41 -0700

    bypass creation class, second room with exit made

--- game/game.py
+++ game/game.py
@@ -14,37 +14,66 @@ class PlayerCreationRoom(Scene):
         print("Welcome to my game! First step, name your character.")
         player = character.Character(input("Name your character!\n>>"))
         print(f"You named them {player.name}. Good Luck!")
-
         return 'firstroom'
-
-
 
 class FirstRoom(Scene):
 
     def enter(self):
-        print("we are in the first room")
-        print("you awake in a room with only one exit. what would you like to do?")
+        print("We are in the first room")
+        print("You awake in a room with only one exit. It's mostly empty, but maybe could find something of use if you search? What would you like to do?")
         action = input ("> ")
         if action == "exit room":
             print("you try to go to the second room")
             return 'secondroom'
 
         if action == "search":
-            print("""You try to find any hints as to what this place is.
+            print("""
+            You try to find any hints as to what this place is.
             While it is clearly not lived in, you dont feel totally unsafe.
             there must be a way out however..
             """)
             return 'firstroom'
 
         else:
+            #have print out valid actions?
             print("invalid action")
             return 'firstroom'
 
 class SecondRoom(Scene):
 
     def enter(self):
-        print("have we made it to the second  room")
-        return exit
+        print("have we made it to the second room")
+        print("""
+        In this room, aside from the door you just came through, there are two others.
+        One of them has a deadbolt lock which is preventing you from getting through.
+        The other is open, but can't quite tell whats in side without going in yourself.
+
+        What do you do?
+        """)
+
+        action = input ("> ")
+        if action == "search":
+            print("""
+            You search the room. 
+            The locked door does have traces of light underneath it. Maybe its the way out?
+            The lock itself could be opened if you have a key.
+            """)
+            return "secondroom"
+
+#       quick test for door logic.
+#       if action == "take key":
+#          player.has_key = True
+#          return "secondroom"
+
+        if action == "open locked door":
+            if player.has_key == False:
+                print("You try to force the door open, but it won't budge.")
+                return 'secondroom'
+
+            if player.has_key == True:
+                print("You insert the key, the deadbolt moves and it is no longer locked!")
+                return exit
+            
 
 
 class Map(object):
@@ -71,6 +100,8 @@ class Map(object):
 #player.has_key = True
 #print(f"do you have the key now? {player.has_key}")
 
-a_map = Map('creation')
+
+a_map = Map('firstroom')
 a_game= engine.Engine(a_map)
+player = character.Character(input("Name your character!\n>>"))
 a_game.play()
