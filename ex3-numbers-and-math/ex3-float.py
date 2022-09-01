670b3a18ed45cc88688e7e8773c7f0cb9fc5bb4b
Casey Trytten <ctrytten@riotgames.com>
2022-09-01 11:50:04 -0700

    rewrite to use floating point

---
+++ ex3-numbers-and-math/ex3-float.py
@@ -0,0 +1,39 @@
+#Rewrite ex3.py to use floating point numbers so it's more accurate. 20.0 is floating point.
+
+#printing statement about counting chickens
+print("I will now count my chickens:")
+
+#print hens, then divide 30 by 6, then add 25
+print("Hens", float(25 + 30 / 6))
+#print roosters, then do: 25 * 3, % 4, then subtract that from 100
+print("Roosters", float(100 - 25 * 3 % 4))
+
+#print the statement
+print("Now I will count the eggs:")
+
+#print the ending value of 4 modluo 2, 1 divided by 4. then sum 3 + 2 + 1 -5 -.25 + 6
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+#print the statement
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#print if true that 3+2 is less than 5-7.
+print(3 + 2 < 5 - 7)
+
+#print statement 3+2, then do math of 3+2
+print("What is 3 + 2?", float(3 + 2))
+#print statement of 5-7, then do the math of 5-7
+print("What is 5 - 7?", float(5 - 7))
+
+#print the statement
+print("Oh, that's why it's False.")
+
+#print the statement
+print("How about some more.")
+
+#print "is it greater", then evaluate if 5 > -2 is true.
+print("Is it greater?", 5 > -2)
+#print "is it greater of equal", then evaluate if 5 is greater than or equal to -2
+print("Is it greater or equal?", 5 >= -2)
+#print "is it less or equal", and evalute if 5 is less than or equal to -2
+print("Is it less or equal?", 5 <= -2)
