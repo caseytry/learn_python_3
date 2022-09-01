1d71878cac823dcf54cd62899edf3558a59adc21
Casey Trytten <ctrytten@riotgames.com>
2022-09-01 11:44:09 -0700

    second part of ex3

---
+++ ex3-numbers-and-math/ex3-maths.py
@@ -0,0 +1,5 @@
+#Find something you need to calculate and write a new .py file that does it.
+
+
+#how many hours in a year, minus how many hours are worked on average
+print( (24 * 365) - (40 * 52))
